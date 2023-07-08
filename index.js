5edfed0e33ec9e2538b8fa021e9619dc93e73b1b
Alexander Pataridze <s.pataridze@gmail.com>
2023-07-09 00:59:54 +0400

    fix: imports

---
+++ index.js
@@ -0,0 +1,3 @@
+import FetchProvider from './src/fetch-provider';
+import useFetch, { useGet, usePost, usePut, usePatch, useDelete } from './src/useFetch';
+export { FetchProvider, useFetch, useGet, usePost, usePut, usePatch, useDelete };
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import FetchProvider from './fetch-provider';
-import useFetch, { useGet, usePost, usePut, usePatch, useDelete } from './useFetch';
-export { FetchProvider, useFetch, useGet, usePost, usePut, usePatch, useDelete };
