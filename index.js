074dbdd75f304dbdc2e34851350f215e3086b54d
Alexander Pataridze <s.pataridze@gmail.com>
2023-08-02 10:06:37 +0400

    add ts configuration

--- index.js
+++
@@ -1,3 +0,0 @@
-import FetchProvider from './src/fetch-provider';
-import useFetch, { useGet, usePost, usePut, usePatch, useDelete } from './src/useFetch';
-export { FetchProvider, useFetch, useGet, usePost, usePut, usePatch, useDelete };
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import FetchProvider from './fetch-provider';
+import useFetch, { useGet, usePost, usePut, usePatch, useDelete } from './useFetch';
+export { FetchProvider, useFetch, useGet, usePost, usePut, usePatch, useDelete };
